247cda64e4e70d84d7f635aed122b118551d1bb4
amerchan <albertomerchanc@hotmail.com>
2018-01-23 12:46:03 -0500

    prueba de conección con API

---
+++ ObackOffice/ObackOffice/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ObackOffice.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string FirstLastName { get; set; }
+        public string SecondLastName { get; set; }
+        public int GenderId { get; set; }
+    }
+}
