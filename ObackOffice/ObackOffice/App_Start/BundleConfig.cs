177a2f2460e2a71deffd59251a8c9a3137e82cbf
amerchan <albertomerchanc@hotmail.com>
2018-01-23 19:05:45 -0500

    Contrucción de Header - Referenciado al issue #1

--- ObackOffice/ObackOffice/App_Start/BundleConfig.cs
+++ ObackOffice/ObackOffice/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace ObackOffice
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/css/fontello.css",
+                      "~/Content/css/estilos.css"));
         }
     }
 }
