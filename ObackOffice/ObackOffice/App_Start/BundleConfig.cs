582cb2bd287fbf182389ade23dd5b927efbad16a
amerchan <albertomerchanc@hotmail.com>
2018-01-27 08:25:56 -0500

    termino de index responsivo con menú estático issue #1

--- ObackOffice/ObackOffice/App_Start/BundleConfig.cs
+++ ObackOffice/ObackOffice/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace ObackOffice
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -22,6 +23,9 @@ namespace ObackOffice
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                    "~/Scripts/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ ObackOffice/ObackOffice/Scripts/main.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(main);
+
+function main() {
+    $('.bt-menu').on('click', function () {
+        $('.contenido').toggleClass('abrir');
+    });
+
+    //Mostramos y ocultamos submenus
+    $('.submenu').click(function () {
+        $(this).children('.children').slideToggle();
+    });
+}
+
+
+
+//var contador = 1;
+//function main() {
+//    $('.bt-menu').click(function () {
+//        if (contador == 1) {
+//            $('.barra-lateral').animate({
+//                left: '0'
+//            });
+//            contador = 0;
+//        } else {
+//            contador = 1;
+//            $('.barra-lateral').animate({
+//                left: '-100%'
+//            });
+//        }
+//    });
+
+//     Mostramos y ocultamos submenus
+//    $('.submenu').click(function () {
+//        $(this).children('.children').slideToggle();
+//    });
+//}
