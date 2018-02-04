d82fdfb58213e4977033f640736dc9c56ff637d0
amerchan <albertomerchanc@hotmail.com>
2018-02-04 17:15:51 -0500

    Íconos para formulario acceso

---
+++ ObackOffice/ObackOffice/Controllers/Administracion/EventoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ObackOffice.Controllers.Administracion
+{
+    public class EventoController : Controller
+    {
+        // GET: Evento
+        public ActionResult Evento()
+        {
+            return View();
+        }
+
+        //public JsonResult GetEventos()
+        //{
+
+        //}
+    }
+}
