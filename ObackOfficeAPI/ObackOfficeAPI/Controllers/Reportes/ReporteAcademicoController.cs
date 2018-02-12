f53b80e9acb1fd9ddb989f35689d0fc157e3db45
jesusefg <jesus89efg@gmail.com>
2018-02-12 12:40:41 -0500

    reporte multiple listo

--- ObackOfficeAPI/ObackOfficeAPI/Controllers/Reportes/ReporteAcademicoController.cs
+++ ObackOfficeAPI/ObackOfficeAPI/Controllers/Reportes/ReporteAcademicoController.cs
@@ -1,13 +1,8 @@
 ﻿using BE.Comun;
 using BL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Web.Hosting;
 using System.Web.Http;
 using System.IO;
-using Newtonsoft.Json;
 
 namespace ObackOfficeAPI.Controllers.Reportes
 {
@@ -40,50 +35,6 @@ namespace ObackOfficeAPI.Controllers.Reportes
             string fullPath = HostingEnvironment.MapPath(@"~/Plantillas Excel/Plantilla Reporte Academico.xlsx");
             FileStream TemplateFile = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
             MemoryStream response = rar.ReporteAcademicoExcel(data,TemplateFile);
-
-            return Ok(response);
-        }
-
-        [HttpPost]
-        public IHttpActionResult BandejaReporteMultiple(BandejaReporteMultiple data)
-        {
-            BandejaReporteMultiple response = rar.BandejaReporteMultiple(data);
-            return Ok(response);
-        }
-
-        [HttpPost]
-        public IHttpActionResult Chart(MultiDataModel data)
-        {
-            string Action = data.String1;
-            List<ReporteMultipleList> Lista = JsonConvert.DeserializeObject<List<ReporteMultipleList>>(data.String2);
-
-            switch (Action)
-            {
-                case "Asistencia":
-                    {
-                        return Ok(Convert.ToBase64String(rar.ChartAsistencia(Lista)));
-                    }
-                case "Aprobados":
-                    {
-                        return Ok(Convert.ToBase64String(rar.ChartAprobados(Lista)));
-                    }
-                case "Promedio":
-                    {
-                        return Ok(Convert.ToBase64String(rar.ChartPromedio(Lista)));
-                    }
-            }
-
-            return BadRequest("No se encontró la acción dentro del controlador.");
-        }
-
-        [HttpPost]
-        public IHttpActionResult BandejaReporteMultipleExcel(MultiDataModel multi)
-        {
-            string fullPath = HostingEnvironment.MapPath(@"~/Plantillas Excel/Plantilla Reporte Academico.xlsx");
-            FileStream TemplateFile = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
-            BandejaReporteMultiple data = JsonConvert.DeserializeObject<BandejaReporteMultiple>(multi.String1);
-
-            MemoryStream response = rar.BandejaReporteMultipleExcel(data, TemplateFile);
 
             return Ok(response);
         }
---
+++ ObackOfficeAPI/ObackOfficeAPI/Controllers/Reportes/ReporteMultipleController.cs
@@ -0,0 +1,62 @@
+﻿using BE.Comun;
+using BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Hosting;
+using System.Web.Http;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ObackOfficeAPI.Controllers.Reportes
+{
+    public class ReporteMultipleController : ApiController
+    {
+        private ReporteMultipleRepository rmr = new ReporteMultipleRepository();
+
+        [HttpPost]
+        public IHttpActionResult BandejaReporteMultiple(BandejaReporteMultiple data)
+        {
+            BandejaReporteMultiple response = rmr.BandejaReporteMultiple(data);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Chart(MultiDataModel data)
+        {
+            string Action = data.String1;
+            List<ReporteMultipleList> Lista = JsonConvert.DeserializeObject<List<ReporteMultipleList>>(data.String2);
+
+            switch (Action)
+            {
+                case "Asistencia":
+                    {
+                        return Ok(Convert.ToBase64String(rmr.ChartAsistencia(Lista)));
+                    }
+                case "Aprobados":
+                    {
+                        return Ok(Convert.ToBase64String(rmr.ChartAprobados(Lista)));
+                    }
+                case "Promedios":
+                    {
+                        return Ok(Convert.ToBase64String(rmr.ChartPromedio(Lista)));
+                    }
+            }
+
+            return BadRequest("No se encontró la acción dentro del controlador.");
+        }
+
+        [HttpPost]
+        public IHttpActionResult BandejaReporteMultipleExcel(MultiDataModel multi)
+        {
+            string fullPath = HostingEnvironment.MapPath(@"~/Plantillas Excel/Plantilla Reporte Academico.xlsx");
+            FileStream TemplateFile = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+            BandejaReporteMultiple data = JsonConvert.DeserializeObject<BandejaReporteMultiple>(multi.String1);
+
+            MemoryStream response = rmr.BandejaReporteMultipleExcel(data, TemplateFile);
+
+            return Ok(response);
+        }
+    }
+}
