554dfeea47e5a952bb37d98350ee4010c5e32f5a
jesusefg <jesus89efg@gmail.com>
2018-02-16 21:16:42 -0500

    programacion de cursos completo... falta estilo

--- ObackOfficeAPI/BE/Administracion/ProgramacionCursos.cs
+++ ObackOfficeAPI/BE/Administracion/ProgramacionCursos.cs
@@ -14,6 +14,7 @@ namespace BE.Administracion
         public int SedeId { get; set; }
         public int EventoId { get; set; }
         public int CursoId { get; set; }
+        public int UsuarioActualizaID { get; set; }
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
         public List<ProgramacionCursosSalones> Salones { get; set; }
@@ -26,6 +27,7 @@ namespace BE.Administracion
         public int SalonId { get; set; }
         public int CapacitadorId { get; set; }
         public int Cupos { get; set; }
+        public int EsEliminado { get; set; }
         public List<ProgramacionCursosClases> Clases { get; set; }
     }
 
@@ -34,6 +36,7 @@ namespace BE.Administracion
         public int RecordType { get; set; }
         public int RecordStatus { get; set; }
         public int ClaseId { get; set; }
+        public int EsEliminado { get; set; }
         public DateTime HoraInicio { get; set; }
         public DateTime HoraFin { get; set; }
     }
