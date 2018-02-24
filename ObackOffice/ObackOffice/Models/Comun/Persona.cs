ea6674f21d24bd3aad5c82cb29c0afdee58811f3
jesusefg <jesus89efg@gmail.com>
2018-02-24 11:31:34 -0500

    fix

--- ObackOffice/ObackOffice/Models/Comun/Persona.cs
+++ ObackOffice/ObackOffice/Models/Comun/Persona.cs
@@ -11,7 +11,7 @@ namespace ObackOffice.Models.Comun
         public string Nombres { get; set; }
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
-        public DateTime FechaNacimiento { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
         public int GeneroId { get; set; }
         public string NroDocumento { get; set; }
         public int TipoDocumentoId { get; set; }
